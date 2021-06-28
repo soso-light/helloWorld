3b341bafeed08559deedd4a0975b35e9e551cbd4
soso-light <80017031+soso-light@users.noreply.github.com>
2021-06-28 15:00:00 +0900

    Main.java - menuChosen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Main {
     private List<Todo> toDoList;
+    private int menuNum;
 
     public static void main(String[] args){
         Main myList = new Main();
@@ -11,7 +12,30 @@ public class Main {
     public void run(){
         toDoList = new ArrayList<>();
         Menu menu = new Menu();
-        menu.printMenu();
-        System.out.println("원하는 메뉴는?");
+        menuNum = menu.chooseMenu();
+        boolean t = true;
+        while(t){
+            t = menuChosen(menuNum);
+        }
+    }
+
+    private boolean menuChosen(int input){
+        switch (input){
+            case 1: readToDo();
+                    break;
+            case 2: //readToDo();
+                    //break;
+            case 3: //readToDo();
+                    //break;
+            case 4: //readToDo();
+                    //break;
+            case 0: return false;
+            default: System.out.println("다시 입력하세요.");
+        }
+        return true;
+    }
+
+    private void readToDo(){
+
     }
 }
